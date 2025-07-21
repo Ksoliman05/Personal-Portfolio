d305dc48efb7353479dbea6af29994b6efbec178
Ksoliman05 <Kerloussoliman05@gmail.com>
2025-07-20 22:26:38 -0400

    Update About.jsx

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './about.css';
+import './About.css';
 import theme_pattern from '../../assets/theme_pattern.svg';
 import profile_img from '../../assets/profile_img.png';
 
